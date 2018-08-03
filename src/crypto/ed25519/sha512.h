51d6e64dbadc8f2907ea3654d67c524fa906cafb
Xule Zhou <other.marcus@icloud.com>
2018-08-04 00:37:37 +0800

    Use mbedtls sha512 for ed25519 module

--- src/crypto/ed25519/sha512.h
+++ src/crypto/ed25519/sha512.h
@@ -4,14 +4,10 @@
 #include <stddef.h>
 
 #include "fixedint.h"
+#include "../sha512.h"
 
 /* state */
-typedef struct sha512_context_ {
-    uint64_t  length, state[8];
-    size_t curlen;
-    unsigned char buf[128];
-} sha512_context;
-
+typedef mbedtls_sha512_context sha512_context;
 
 int sha512_init(sha512_context * md);
 int sha512_final(sha512_context * md, unsigned char *out);
