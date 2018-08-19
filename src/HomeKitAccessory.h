67cf17e35dde9fe58f33dbc60742609d9cb5d952
Xule Zhou <other.marcus@icloud.com>
2018-08-19 11:57:10 -0400

    Add README.MD

--- src/HomeKitAccessory.h
+++ src/HomeKitAccessory.h
@@ -1,3 +1,28 @@
+/**
+ * hapd
+ *
+ * Copyright 2018 Xule Zhou
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include <utility>
 #include "common.h"
 
@@ -251,7 +276,7 @@ private:
     friend class HAPPairingsManager;
 
     void * mdns_handle = nullptr;
-    const char * deviceId = "F6:A4:35:E3:0B:09";
+    const char * deviceId = "F6:A4:35:E3:0B:0b";
     const char * setupCode = "816-32-958";
 };
 
--- src/testings.cpp
+++ src/testings.cpp
@@ -1,3 +1,28 @@
+/**
+ * hapd
+ *
+ * Copyright 2018 Xule Zhou
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include "HomeKitAccessory.h"
 #include "service/Switch.h"
 
@@ -19,7 +44,9 @@ int main(){
     }
 }
 
-#else
+#endif
+
+#ifdef HAP_ESP8266_DEBUG_ENV
 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
@@ -27,7 +54,7 @@ int main(){
 void setup(){
     Serial.begin(115200);
     Serial.setDebugOutput(true);
-    WiFi.begin("Xiaomi_CJ", "86028560");
+    WiFi.begin();
     HAP_DEBUG("wlan connecting");
 
     while (WiFi.status() != WL_CONNECTED) {
